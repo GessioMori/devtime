d47f018c990634fb66883f8199f9315356c2fe1d
Gessio Mori <gessiomorin@gmail.com>
2022-10-27 09:26:34 -0300

    adds type of chart selection to task stats page

--- src/pages/dashboard/statistics/tasks.tsx
+++ src/pages/dashboard/statistics/tasks.tsx
@@ -1,10 +1,33 @@
 import { TaskNumberByProject } from '@/components/statistics/tasks/TaskNumberByProject';
-import { Container } from '@mantine/core';
+import { Container, Select, Text } from '@mantine/core';
+import { useState } from 'react';
+
+type selectionOpts = string | null;
 
 const TasksStatisticsPage = () => {
+  const [selection, setSelection] = useState<selectionOpts | null>(null);
+
+  console.log(selection);
+
   return (
     <Container sx={{ height: '80vh' }}>
-      <TaskNumberByProject />
+      <Select
+        value={selection}
+        placeholder={'Select a chart to show'}
+        onChange={setSelection}
+        data={[
+          { value: 'taskNumberByProject', label: 'Number of tasks by project' },
+          {
+            value: 'taskTimeByProject',
+            label: 'Time spent on tasks for each project'
+          }
+        ]}
+      />
+      {selection === 'taskNumberByProject' ? (
+        <TaskNumberByProject />
+      ) : (
+        <Text>No data</Text>
+      )}
     </Container>
   );
 };
